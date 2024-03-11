186a49430483a4b25eae12114f5f06c7d67327c1
HaDangTu <38284966+HaDangTu@users.noreply.github.com>
2024-03-11 08:17:39 +0700

    [Tu Ha] Add ipx for image optimizer

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 const path = require('path');
 const express = require('express');
+const { createIPX, ipxFSStorage, createIPXNodeServer } = require('ipx');
+
+const ipx = createIPX({
+  storage: ipxFSStorage({ dir: path.join(__dirname, '../public/assets/images')}),
+});
+const ipxMiddleware = createIPXNodeServer(ipx);
 
 const app = express();
 
+app.use('/photos', ipxMiddleware);
+
 app.use(express.static(path.join(__dirname, '../', 'public')));
+
 
 app.listen(3000, () => {
   console.log('[INFO] Dev server start at: http://localhost:3000');
